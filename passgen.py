6f16454e64a200e719f136eca03f7e85b30adda7
Ayush Satyavarpu <ayushsatyavarpu@gmail.com>
2025-08-18 10:21:38 -0700

    updated to use sys args

--- passgen.py
+++ passgen.py
@@ -1,11 +1,14 @@
 import hashlib
+import sys
 
 def sha256_hash(text: str) -> str:
     return hashlib.sha256(text.encode()).hexdigest()
 
-
 if __name__ == "__main__":
-    root_key = input("Enter root key: ")
-    descriptor = input("Enter descriptor: ")
-    print("Password is: ", sha256_hash(root_key + descriptor)[:10])
-    
+    if len(sys.argv) != 3:
+        print(f"Usage: python {sys.argv[0]} <root_key> <descriptor>")
+        sys.exit(1)
+
+    root_key = sys.argv[1]
+    descriptor = sys.argv[2]
+    print("Password is:", sha256_hash(root_key + descriptor)[:10])
